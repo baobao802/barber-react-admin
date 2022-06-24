fe1ffb981ee3a3754120968c3109e8ba818a9d6d
baobao802 <88592501+baobao802@users.noreply.github.com>
2022-06-25 00:07:47 +0700

    Update notificationsMapper.js

--- src/modules/notifications/utils/mappers/notificationsMapper.js
+++ src/modules/notifications/utils/mappers/notificationsMapper.js
@@ -10,6 +10,6 @@ export default function notificationsMapper(notifications) {
 
   return {
     notifications: newNotifications || [],
-    totalPages: notifications.totalPage,
+    totalPages: notifications.totalPages,
   };
 }
